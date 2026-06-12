6f2a5206a195afa0ee12577388a7071ce3e32fca
Payal Sulaniya <payalsulaniya88@gmail.com>
2026-06-13 01:07:25 +0530

    added explanation for the question

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -4,6 +4,7 @@ public:
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
 
+        //here first thing is i=0; and j should i+1. j>i+1 so that will help in removing duplicates. Use long long for longer valued test cases. 
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
                 if(i > 0 && nums[i] == nums[i-1]) continue;
