5c7d5fa8b4d30de36d938fdda55b431b7e65a1f7
Payal Sulaniya <payalsulaniya88@gmail.com>
2026-06-24 02:54:11 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (15.59%) - LeetSync

---
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        //kind of traverse the list and thn check the elemt if the next elemt is equal then change the prev pointer to the next value like remove the elemts until a new diff elemt comes up
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode* prev = &dummy;
+        ListNode* curr = head;
+
+        while(curr != NULL){
+            if(curr->next && curr ->val == curr->next->val){
+                int value = curr->val;
+
+                while(curr && curr->val == value){
+                    curr = curr->next;
+                }
+                prev->next = curr;
+            }
+            else{
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        return dummy.next;
+    }
+};
