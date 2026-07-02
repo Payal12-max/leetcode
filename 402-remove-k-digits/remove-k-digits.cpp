7b736f5458db2258e96a3431ef6bb180441c7356
Payal Sulaniya <payalsulaniya88@gmail.com>
2026-07-03 02:16:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (74.42%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string st;
+
+        for (char digit : num) {
+            while (!st.empty() && k > 0 && st.back() > digit) {
+                st.pop_back();
+                k--;
+            }
+            st.push_back(digit);
+        }
+
+        // If k is still left, remove digits from end
+        while (k > 0 && !st.empty()) {
+            st.pop_back();
+            k--;
+        }
+
+        // Remove leading zeroes
+        int i = 0;
+        while (i < st.size() && st[i] == '0') {
+            i++;
+        }
+
+        string ans = st.substr(i);
+
+        return ans.empty() ? "0" : ans;
+    }
+};
