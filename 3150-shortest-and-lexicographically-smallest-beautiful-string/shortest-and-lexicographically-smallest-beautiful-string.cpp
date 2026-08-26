da4f20ab14c9a7db9fa9bb98fd29486a02bc17ef
Payal Sulaniya <payalsulaniya88@gmail.com>
2026-08-27 04:10:03 +0530

    Time: 3 ms (26.85%) | Memory: 8.7 MB (64.71%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//the brute force appraoch-
+/*class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
         vector<string> ans;
@@ -21,6 +22,47 @@ public:
                 result = sub;
             }
         } 
+        return result;
+    }
+};*/
+
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.length();
+
+        int left = 0;
+        int ones = 0;
+
+        string result = "";
+
+        for (int right = 0; right < n; right++) {
+
+            // Add current character
+            if (s[right] == '1')
+                ones++;
+
+            // If we have exactly k ones,
+            // try shrinking from the left
+            while (ones == k) {
+
+                string sub = s.substr(left, right - left + 1);
+
+                // Update answer
+                if (result == "" ||
+                    sub.length() < result.length() ||
+                    (sub.length() == result.length() && sub < result)) {
+                    result = sub;
+                }
+
+                // Remove left character
+                if (s[left] == '1')
+                    ones--;
+
+                left++;
+            }
+        }
+
         return result;
     }
 };
